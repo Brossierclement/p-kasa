8d9792733f4d35ac7e54e99e6298981226e7e601
Clément Brossier <clement.audiovisuel@gmail.com>
2023-07-26 15:43:16 +0200

    media query for Form it's ok

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -14,27 +14,23 @@ function Description() {
   const card = housing.find((item) => item.id === id);
   return (
     <div id="description">
-      <div id="descriptionHeader">
-        <div id="descriptionTitleLocation">
-          <h2>{card.title}</h2>
-          <p>{card.location}</p>
-        </div>
-        <div id="descriptionNamePicture">
-          <p>{card.host.name}</p>
-          <img src={card.host.picture} alt="profil" />
-        </div>
+      <div id="descriptionTitleLocation">
+        <h2>{card.title}</h2>
+        <p>{card.location}</p>
       </div>
-      <div id="tagAndRating">
-        <div id="descriptionTags">
-          {card.tags.map((tag, index) => (
-            <Tag key={index} tags={tag} />
-          ))}
-        </div>
-        <div id="descriptionRating">
-          <Stars star={greyStar} rating={card.rating} filledStar={redStar} />
-        </div>
+      <div id="descriptionNamePicture">
+        <p>{card.host.name}</p>
+        <img src={card.host.picture} alt="profil" />
       </div>
-      <div id="descriptionDropdown">
+      <div id="descriptionTags">
+        {card.tags.map((tag, index) => (
+          <Tag key={index} tags={tag} />
+        ))}
+      </div>
+      <div id="descriptionRating">
+        <Stars star={greyStar} rating={card.rating} filledStar={redStar} />
+      </div>
+      <div className="descriptionDropdown">
         <Dropdown title="Description" text={card.description} />
         <Dropdown title="Equipements" equipments={card.equipments} />
       </div>
